a269a14592a0324147ea3236e24771bb26828715
ShibePlanet <alelo2006@live.com>
2022-08-06 17:50:49 -0400

    Add Bow Textures And Recipes

--- src/main/java/io/github/shibeplanet/infernium/init/ItemInit.java
+++ src/main/java/io/github/shibeplanet/infernium/init/ItemInit.java
@@ -1,6 +1,5 @@
 package io.github.shibeplanet.infernium.init;
 
-
 import io.github.shibeplanet.infernium.Infernium;
 import io.github.shibeplanet.infernium.items.AdvBowItem;
 import net.minecraft.network.chat.Component;
@@ -26,6 +25,14 @@ public class ItemInit {
     // Note: wooden bow
     //       baseArrowDamage: 2.0d
     //       drawSpeed: 20.0f
+
+    /* Reinforced String */
+    public static final RegistryObject<Item> REINFORCED_STRING = ITEMS.register("reinforced_string",
+            () -> new Item(new Item.Properties()
+                    .tab(CreativeModeTab.TAB_MISC)
+            ));
+
+
     /* Stone Bow */
     public static final RegistryObject<AdvBowItem> STONE_BOW = ITEMS.register("stone_bow",
             () -> new AdvBowItem(2.5d, 20.0f, new Item.Properties()
@@ -62,7 +69,7 @@ public class ItemInit {
             ));
 
     //// Infernium Items
-    /*Infernium Ingot*/
+    /* Infernium Ingot */
     public static final RegistryObject<Item> INFERNIUM_INGOT = ITEMS.register("infernium_ingot",
             () -> new Item(new Item.Properties()
                     .tab(Infernium.TAB)
@@ -81,7 +88,7 @@ public class ItemInit {
                 }
     });
 
-    /*Infernium Sword*/
+    /* Infernium Sword */
     public static final RegistryObject<SwordItem> INFERNIUM_SWORD = ITEMS.register("infernium_sword",
             () -> new SwordItem(Tiers.TIERFIVE, 4 /*ATK dmg*/, -2.2f /*ATK spd*/, new Item.Properties()
                     .tab(Infernium.TAB)
@@ -100,7 +107,7 @@ public class ItemInit {
                 }
     });
 
-    /*Infernium Pickaxe*/
+    /* Infernium Pickaxe */
     public static final RegistryObject<PickaxeItem> INFERNIUM_PICKAXE = ITEMS.register("infernium_pickaxe",
             () -> new PickaxeItem(Tiers.TIERFIVE, 2 /*ATK dmg*/, -2.6f /*ATK spd*/, new Item.Properties()
                     .tab(Infernium.TAB)
@@ -119,7 +126,7 @@ public class ItemInit {
                 }
             });
 
-    /*Infernium Axe*/
+    /* Infernium Axe */
     public static final RegistryObject<AxeItem> INFERNIUM_AXE = ITEMS.register("infernium_axe",
             () -> new AxeItem(Tiers.TIERFIVE, 6 /*ATK dmg*/, -2.8f /*ATK spd*/, new Item.Properties()
                     .tab(Infernium.TAB)
@@ -138,7 +145,7 @@ public class ItemInit {
                 }
             });
 
-    /*Infernium Shovel*/
+    /* Infernium Shovel */
     public static final RegistryObject<ShovelItem> INFERNIUM_SHOVEL = ITEMS.register("infernium_shovel",
             () -> new ShovelItem(Tiers.TIERFIVE, 2.5f /*ATK dmg*/, -2.8f /*ATK spd*/, new Item.Properties()
                     .tab(Infernium.TAB)
@@ -157,7 +164,7 @@ public class ItemInit {
                 }
             });
 
-    /*Infernium Hoe*/
+    /* Infernium Hoe */
     public static final RegistryObject<HoeItem> INFERNIUM_HOE = ITEMS.register("infernium_hoe",
             () -> new HoeItem(Tiers.TIERFIVE, -4 /*ATK dmg*/, -3f /*ATK spd*/, new Item.Properties()
                     .tab(Infernium.TAB)
@@ -180,7 +187,7 @@ public class ItemInit {
 
 
     //// Nihilium Items
-    /*Dragon Scale*/
+    /* Dragon Scale */
     public static final RegistryObject<Item> DRAGON_SCALE = ITEMS.register("dragon_scale",
             () -> new Item(new Item.Properties()
                     .tab(Infernium.TAB)
@@ -208,7 +215,7 @@ public class ItemInit {
 
 
     //// Developer Items
-    /*One Millimeter Defeater*/
+    /* One Millimeter Defeater */
     public static final RegistryObject<SwordItem> ONE_MM_DEFEATER = ITEMS.register("one_mm_defeater",
             () -> new SwordItem(Tiers.TIERMM, 1000 /*ATK dmg*/, 1000.0f /*ATK spd*/, new Item.Properties()
                     .tab(Infernium.TAB)
